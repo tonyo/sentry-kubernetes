1807c0e5b2671f8ca881daf9f7d4aadc93ab6e65
Jack Zhang <jack.zhang@sentry.io>
2023-11-14 11:42:06 -0800

    add checks for Sentry tags in unit test

--- watcher_pods_test.go
+++ watcher_pods_test.go
@@ -10,10 +10,16 @@ import (
 	"k8s.io/apimachinery/pkg/watch"
 )
 
+// test the function handlePodWatchEvent
+// by giving it a mock pod with a status
+// and checking it causes Sentry to capture the event
+// with the correct message and Sentry tags
 func TestHandlePodWatchEvent(t *testing.T) {
 
+	// Create empty context
 	ctx := context.Background()
 
+	// Define an SDK transport that only captures events but not send them
 	transport := &TransportMock{}
 	client, err := sentry.NewClient(sentry.ClientOptions{
 		Transport: transport,
@@ -24,13 +30,16 @@ func TestHandlePodWatchEvent(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Create new scope
 	scope := sentry.NewScope()
-
+	// Create hub with new scope
 	hub := sentry.NewHub(client, scope)
-
+	// Attach the hub to the empty context
 	ctx = sentry.SetHubOnContext(ctx, hub)
 
-	// create the event which includes the mock pod
+	// Create the watch event which includes the mock pod
+	// where the pod includes statuses to create Sentry events from
 	mockEvent := watch.Event{
 		Type: watch.Modified,
 		Object: &corev1.Pod{
@@ -55,18 +64,38 @@ func TestHandlePodWatchEvent(t *testing.T) {
 		},
 	}
 
-	// process the events
+	// The function that is tested
+	// in which it should capture the mock event
+	// and produce a Sentry event with the correct
+	// message and corresponding Sentry tags
 	handlePodWatchEvent(ctx, &mockEvent)
 
+	// Only a single event should be created corresponding
+	// to the single container status in the mock pod
 	expectedNumEvents := 1
-	expectedMsg := "Fake Message: TestHandlePodWatchEvent"
-
 	events := transport.Events()
 	if len(events) != 1 {
 		t.Errorf("received %d events, expected %d event", len(events), expectedNumEvents)
 	}
 
+	// the Sentry event message should match that of the container status
+	expectedMsg := "Fake Message: TestHandlePodWatchEvent"
 	if events[0].Message != expectedMsg {
 		t.Errorf("received %s, wanted %s", events[0].Message, expectedMsg)
+	}
+
+	// Check that the tags of event are set correctly by the innermost scope
+	// corresponding to the creation of the Sentry event
+	expectedTags := map[string]string{"container_name": "fake_DNS_Label",
+		"event_source_component": "x-pod-controller",
+		"namespace":              "TestHandlePodWatchEventNameSpace",
+		"pod_name":               "TestHandlePodWatchEventPod",
+		"reason":                 "Fake Reason: TestHandlePodWatchEvent",
+		"watcher_name":           "pods"}
+	// the test fails if any tag key, value pair does not match
+	for key, val := range expectedTags {
+		if events[0].Tags[key] != expectedTags[key] {
+			t.Errorf("For Sentry tag with key [%s], received \"%s\", wanted \"%s\"", key, events[0].Tags[key], val)
+		}
 	}
 }
